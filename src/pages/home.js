7b827a0ad53bc233b57469e3e9201d1093c9222b
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-03-31 00:09:45 -0300

    header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { OptForm } from "../components";
+import { Feature } from "../components";
 
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
@@ -9,10 +11,27 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
     </>
   );
 }
