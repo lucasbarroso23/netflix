7f2bdc7e6ab33cc0e37297a91182b060450ec459
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-01 23:54:57 -0300

    protecting routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import * as ROUTES from './constants/routes';
-import {Home, Signin, Signup, Browse} from './pages';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Home, Signin, Signup, Browse } from "./pages";
+import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = null;
   return (
     <Router>
-      <Route exact path={ROUTES.HOME} component={Home} />
-      <Route path={ROUTES.BROWSE} component={Browse} />
-      <Route path={ROUTES.SIGN_IN} component={Signin} />
-      <Route path={ROUTES.SIGN_UP} component={Signup} />
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_IN}
+        exact
+      >
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.SIGN_UP}
+        exact
+      >
+        <Signup />
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+        exact
+      >
+        <Home />
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>      
     </Router>
   );
 }
