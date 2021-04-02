a843cf8190d38e8ec13882810de05afa353c475b
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-01 22:38:55 -0300

    firebase signin auth setup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
-import { Form } from "../components";
+import React, { useState, useContext } from "react";
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "../containers/footer";
 import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
-  const [emailAdress, setEmailAdress] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAdress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const isInvalid = password === '' || emailAdress === '';
+  const isInvalid = password === "" || emailAdress === "";
 
   const handleSignin = (event) => {
     event.preventDefault();
 
     //firebase work here
-  }
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAdress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAdress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
 
   return (
     <>
@@ -41,11 +57,11 @@ export default function Signin() {
             </Form.Submit>
           </Form.Base>
           <Form.Text>
-            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link> 
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
           </Form.Text>
           <Form.TextSmall>
-            This page is protected by Google reCAPTCHA to ensure
-            you're not a bot. Learn more.
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
           </Form.TextSmall>
         </Form>
       </HeaderContainer>
