bdaf8cbad26d473022e41c26efbdf6ada4b42487
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-03 00:11:16 -0300

    get firebase data

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React from "react";
+import { useContent } from "../hooks";
 
 export default function Browse() {
+  // we need the series and the films
+  const { series } = useContent("series");
+  const { films } = useContent("films");
+
+  console.log('series', series);
+  console.log('films', films);
+
+  // we need slides
+  // pass it to the browse container
+
   return <p>Hello from the browse!</p>;
 }
