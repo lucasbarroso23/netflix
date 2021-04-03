2510c8c89d92dff965ac0c46125337eee9ebc6d9
lucasbarroso23 <lucasbarroso23@gmail.com>
2021-04-03 11:43:19 -0300

    loading spinner

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,11 +1,34 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { SelectProfileContainer } from './profiles';
 import { FirebaseContext } from '../context/firebase';
-
+import { Header, Loading } from '../components';
+ 
 export function BrowseContainer({ slides }) {
   const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext)
   const user = firebase.auth().currentUser || {};
 
-  return <SelectProfileContainer user={user} setProfile={setProfile} />
+  useEffect(() => {
+    console.log(profile);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile, profile.displayName])
+
+  return profile.displayName ? (
+    <>
+   { loading ? (
+    <Loading src={user.photoURL} />
+    ) : (
+    <Loading.ReleaseBody />
+    )}
+
+    <Header src="joker1">
+      <h1>hello</h1>
+    </Header>
+    </>
+  ) : (
+  <SelectProfileContainer user={user} setProfile={setProfile} />
+  )
 }
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Header, Profiles } from '../components';
-import * as ROUTES from '../constants/routes';
-import logo from '../logo.svg'; 
+import React from "react";
+import { Header, Profiles } from "../components";
+import * as ROUTES from "../constants/routes";
+import logo from "../logo.svg";
 
 export function SelectProfileContainer({ user, setProfile }) {
   return (
@@ -15,7 +15,14 @@ export function SelectProfileContainer({ user, setProfile }) {
       <Profiles>
         <Profiles.Title>Who's watching?</Profiles.Title>
         <Profiles.List>
-          <Profiles.User>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
             <Profiles.Picture src={user.photoURL} />
             <Profiles.Name>{user.displayName}</Profiles.Name>
           </Profiles.User>
